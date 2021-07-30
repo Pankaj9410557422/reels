525bfdb7dfd84adb4d6eca25add471d9f1740e6e
Pankaj <engg.pankaj98@gmail.com>
2021-07-30 20:19:11 +0530

    gitignore updated

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
+import {database} from './firebaseData';
+import {makeStyles} from '@material-ui/core/styles'
+import {CircularProgress} from '@material-ui/core';
+import {Avatar} from "@material-ui/core";
+import './Comments.css'
+const useStyles= makeStyles({
+    da:{
+        marginRight:"2%",
+        marginTop:"2%"
+    }
+})
 
-function Comments() {
+function Comments(props) {
+    const classes=useStyles();
+    const [comments, setComments] = useState(null);
+    useState(async()=>{
+        let arr=[];
+        for(let i=0; i<props.postData.comments.length;i++){
+            let cId = props.postData.comments[i];
+            let data = await database.comments.doc(cId).get();
+            arr.push(data.data());
+        }
+        console.log(arr);
+        setComments(arr);
+    },[props.postData])
     return (
-        <div>
-            
-        </div>
+        <>
+        {
+            comments==null? <CircularProgress/> :
+                comments.map((comment, index)=>(
+                    <div key={index} className="comment-div">
+                        <Avatar src={comment.uUrl} className={classes.da}/>
+                        <p><span style={{fontWeight:"bold",display:"inline-block"}}>{comment.uName}</span>&nbsp;&nbsp;{comment.text}</p>
+                    </div>
+                ))
+        }
+        </>
+        
     )
 }
 
